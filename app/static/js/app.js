d408458594f809c1c3ea6917d9f54810a627f20b
jonasdebeukelaer <jonas.db@live.co.uk>
2015-08-17 19:31:13 +0100

    Added all requirements

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,9 +1,44 @@
 // Declare app level module which depends on filters, and services
-angular.module('unisalad', ['ngResource', 'ngRoute', 'ui.bootstrap', 'ui.date'])
+angular.module('unisalad', [
+	'ngResource',
+	'ngRoute',
+	'ui.bootstrap',
+	'ui.date',
+  'ngTouch',
+  'LocalStorageModule',
+  'ngAnimate',
+  'ngMaterial'
+	])
   .config(['$routeProvider', function ($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/home/home.html', 
         controller: 'HomeController'})
-      .otherwise({redirectTo: '/'});
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .when('/tool', {
+        templateUrl: 'views/tool.html',
+        controller: 'ToolCtrl'
+      })
+      .when('/login', {
+        templateUrl: 'views/login.html',
+        controller: 'LoginCtrl'
+      })
+      .when('/listview', {
+        templateUrl: 'views/listview.html',
+        controller: 'ListviewCtrl'
+      })
+      .when('/addpost', {
+        templateUrl: 'views/addpost.html',
+        controller: 'AddpostCtrl'
+      })
+      .when('/confirmationsent', {
+        templateUrl: 'views/confirmationsent.html',
+        controller: 'ConfirmationsentCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
   }]);
