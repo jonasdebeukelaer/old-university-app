c7fb19f9c42a5c767352d5ab3cd4a8c351bf4e10
Richard Jones <joneseh25@gmail.com>
2015-11-21 12:29:02 +0000

    Added a bunch of user functionality including password hashes, login, verification, etc. Also added getting all universities.

--- app/models/User.py
+++ app/models/User.py
@@ -1,17 +1,36 @@
 from app import db
+from app.utils import password
 
 class User(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	username = db.Column(db.String(64), index=True, unique=True)
 	email = db.Column(db.Text, index=True, unique=True)
 	password = db.Column(db.String(128))
-	upVotes = db.Column(db.Integer)
-	downVotes = db.Column(db.Integer)
-	phoneNumber = db.Column(db.String(15))
 	fullName = db.Column(db.Text)
-	emailContactable = db.Column(db.Boolean)
-	phoneContactable = db.Column(db.Boolean)
+	phoneNumber = db.Column(db.String(15))
+
+	upVotes = db.Column(db.Integer, default=0)
+	downVotes = db.Column(db.Integer, default=0)
+	emailContactable = db.Column(db.Boolean, default=False)
+	phoneContactable = db.Column(db.Boolean, default=False)
+	verified = db.Column(db.Boolean, default=False)
 	# posts = db.relationship("Post", backref = "user", lazy = "dynamic")
+
+	def __init__(self, form):
+		self.username = form['username']
+		self.email = form['email']
+		self.fullName = form['fullName']
+
+		givenPassword = form['password']
+		self.password = password.hashPassword(givenPassword)
+
+		# TODO: Verify that phone number is valid for country?
+		self.phoneNumber = form['phoneNumber']
+
+		if('emailContactable' in form):
+			self.emailContactable = form['emailContactable']
+		if('phoneContactable' in form):
+			self.phoneContactable = form['phoneContactable']
 
 	def __repr__(self):
 		return '<User: %r>' % (self.username)
