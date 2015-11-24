8775462f623deb11dfb305bac1d9d1ed4ba91902
jonasdebeukelaer <jonas.db@live.co.uk>
2015-11-24 22:29:16 +0000

    tidied up mod page to resemble normal post feed, will add functionally later

--- app/static/js/controllers/mod_page.js
+++ app/static/js/controllers/mod_page.js
@@ -4,6 +4,7 @@ angular.module('unisalad')
   .controller('ModPageCtrl', function ($scope) {
   	$scope.pageClass = "mod-page";
   	$scope.examplePost = {
+      "type":"ticket",
   		"id": 1,
     	"item": "Crisis ticket",
     	"postDate": "2015-03-20",
@@ -14,4 +15,8 @@ angular.module('unisalad')
     	"evenDate": "2015-03-25",
       "extraInfo": "Extra info about the ticket goes here"
   	}
+
+    $scope.list = function () {
+      return "ticket"
+    }
   });
