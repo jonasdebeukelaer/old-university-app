3d631cda23eaccb52e699c6cb87d3d040a276fde
Jonas De Beukelaer <jonas.debeukelaer@underwriteme.co.uk>
2016-08-16 18:10:47 +0100

    explicit get to fix heroku?...

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
+const fs = require('fs');
 const https = require('https');
+const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 
 const conf = require('./conf');
+const routes = require('./routes');
 
+
+const credentials = {
+    key: fs.readFileSync(conf.cert.keypath, 'utf8'),
+    cert: fs.readFileSync(conf.cert.certpath, 'utf8')
+};
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(morgan('dev')); 
 
-app.use(express.static('static'));
+app.use(express.static(__dirname + '/static'));
+app.use('/api', routes);
 
-https.createServer({}, app).listen(process.env.PORT || conf.port);
+app.get('/',function(request,response){
+       response.sendFile(__dirname+'static/index.html');
+    });
+
+https.createServer(credentials, app).listen(process.env.PORT || conf.port);
 
 console.log('Started server on port ' + (process.env.PORT || conf.port));
