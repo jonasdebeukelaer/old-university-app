296c180d47a9957be2611b1e371a6d433e52fc9f
Jonas De Beukelaer <jonas.debeukelaer@underwriteme.co.uk>
2016-08-16 15:16:44 +0100

    still trying to fix heroku

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-// const cors = require('cors');  //TODO remove in prod
-
 const conf = require('./conf');
 const routes = require('./routes');
 
@@ -18,24 +16,7 @@ const credentials = {
 const app = express();
 
 app.use(bodyParser.json());
-// app.use(cors()); //TODO remove in prod
-// app.use(function (req, res, next) {
-// 	// Website you wish to allow to connect
-//     res.setHeader('Access-Control-Allow-Origin', 'http://0.0.0.0:9000');
 
-//     // Request methods you wish to allow
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-//     // Request headers you wish to allow
-//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-//     // Set to true if you need the website to include cookies in the requests sent
-//     // to the API (e.g. in case you use sessions)
-//     res.setHeader('Access-Control-Allow-Credentials', true);
-
-//     // Pass to next layer of middleware
-//     next();
-// })
 app.use('/api', routes);
 
 https.createServer(credentials, app).listen(process.env.PORT || conf.port);
