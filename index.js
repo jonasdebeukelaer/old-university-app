bb9b655cb2d2852ad6fa61f9c13fecfd2657daa8
Jonas De Beukelaer <jonas.debeukelaer@underwriteme.co.uk>
2016-08-16 18:16:37 +0100

    now created minimal index for heroku plssss

--- index.js
+++ index.js
@@ -1,31 +1,15 @@
-const fs = require('fs');
-const https = require('https');
-const http = require('http');
-const express = require('express');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
 
-const conf = require('./conf');
-const routes = require('./routes');
-
-
-const credentials = {
-    key: fs.readFileSync(conf.cert.keypath, 'utf8'),
-    cert: fs.readFileSync(conf.cert.certpath, 'utf8')
-};
-
-const app = express();
-
-app.use(bodyParser.json());
-app.use(morgan('dev')); 
+var express = require("express");
+var app     = express();
+var path    = require("path");
 
 app.use(express.static(__dirname + '/static'));
-app.use('/api', routes);
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+});
 
-app.get('/',function(request,response){
-       response.sendFile(__dirname+'static/index.html');
-    });
+app.listen(process.env.PORT ||  3000);
 
-https.createServer(credentials, app).listen(process.env.PORT || conf.port);
 
-console.log('Started server on port ' + (process.env.PORT || conf.port));
+
+console.log("Running at Port " + (process.env.PORT || "3000"));
