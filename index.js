6c0cffe96ff4f29e91fe5d90c49248779a4e3314
Jonas De Beukelaer <jonas.debeukelaer@underwriteme.co.uk>
2016-08-16 18:20:50 +0100

    removed https to see if helps

--- index.js
+++ index.js
@@ -1,15 +1,46 @@
+const fs = require('fs');
+const https = require('https');
+const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
 
-var express = require("express");
-var app     = express();
-var path    = require("path");
+const conf = require('./conf');
+const routes = require('./routes');
+
+
+const credentials = {
+    key: fs.readFileSync(conf.cert.keypath, 'utf8'),
+    cert: fs.readFileSync(conf.cert.certpath, 'utf8')
+};
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(morgan('dev')); 
 
 app.use(express.static(__dirname + '/static'));
 app.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/index.html'));
 });
 
-app.listen(process.env.PORT ||  3000);
+app.use('/api', routes);
+
+app.listen(process.env.PORT || conf.port);
+
+console.log('Started server on port ' + (process.env.PORT || conf.port));
+
+// var express = require("express");
+// var app     = express();
+// var path    = require("path");
+
+// app.use(express.static(__dirname + '/static'));
+// app.get('/',function(req,res){
+//   res.sendFile(path.join(__dirname+'/index.html'));
+// });
+
+// app.listen(process.env.PORT ||  3000);
 
 
 
-console.log("Running at Port " + (process.env.PORT || "3000"));
+// console.log("Running at Port " + (process.env.PORT || "3000");
