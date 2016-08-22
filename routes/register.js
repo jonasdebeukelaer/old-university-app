bf9c9e69619010b9f71c21b50cda69a067317143
Jonas De Beukelaer <jonas.debeukelaer@underwriteme.co.uk>
2016-08-22 14:18:16 +0100

    renamings and trying to get more stacktrace displayed on node errors

--- routes/register.js
+++ routes/register.js
@@ -22,12 +22,16 @@ function sendMail(user) {
 function setup(req, res) {
     const pwd = req.body.password;
     const email = req.body.email;
-    console.log(email + "\t" + pwd)
-    db.emailNotInUse(email)
-        .then(() => db.createUser(email, pwd))
-        .then(sendMail)
-        .then(user => res.status(200).send(user.id))
-        .catch(err => res.status(500).send(err));
+    console.log("Inside register:\t" + email + "\t" + pwd)
+    try {
+        db.emailNotInUse(email)
+            .then(() => db.createUser(email, pwd))
+            //.then(sendMail)
+            .then(user => res.status(200).send(user.id))
+            .catch(err => res.status(500).send(err));
+    } catch  (e) {
+        console.error(e.stack);
+    }
 }
 
 function verify(req, res) {
